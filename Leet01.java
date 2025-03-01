a34577b085c1409e453bc96486626ef3ff97627c
Harshit (s) Rautela <138342633+rrrautela@users.noreply.github.com>
2025-03-01 22:26:12 +0530

    Add files via upload

--- Leet01.java
+++ Leet01.java
@@ -1,2 +1,19 @@
+
+//1 March 2025
+//reverse a string
 public class Leet01 {
+    public static void reverseString(StringBuilder s) {
+        if(s.length() == 0) return;
+
+        char ch  = s.charAt(0);
+        s.deleteCharAt(0);
+        reverseString(s);
+        s.append(ch);
+    }
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder();
+        s.append("hello darkness my old friend");
+        reverseString(s);
+        System.out.println(s);
+    }
 }
